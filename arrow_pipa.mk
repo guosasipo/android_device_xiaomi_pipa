2d016beefd99594f4a028ef404ca255888e7df87
guosasipo <leehojoonlee0820@gmail.com>
2023-07-02 13:11:35 +0900

    pipa: add arrow specific things

--- arrow_pipa.mk
+++ arrow_pipa.mk
@@ -14,13 +14,19 @@ $(call inherit-product, device/xiaomi/pipa/device.mk)
 # Inherit common LineageOS configurations
 $(call inherit-product, vendor/arrow/config/common.mk)
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
 PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_CHARACTERISTICS := nosdcard
+ARROW_GAPPS := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 23043RP34C
 PRODUCT_NAME := arrow_pipa
+DEVICE_MAINTAINER := Guosasipo
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
